369066868ac9b488ad5647d98f8bf49441a601d5
boulhouech <slim.boulahouech@gmail.com>
2024-05-21 21:54:38 +0100

    changing the logic in the Board class

--- main.rb
+++ main.rb
@@ -1,11 +1,27 @@
 Class Board
-@@board = array.new(9,"-")
-def self.display_board
-  puts "#{@@board[0]} | #{@@board[1]} | #{@@board[2]}"
-  puts "----------"
-  puts "#{@@board[3]} | #{@@board[4]} | #{@@board[5]}"
-  puts "----------"
-  puts "#{@@board[6]} | #{@@board[7]} | #{@@board[8]}"
+def initialize
+  super
+  @board_position = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+end
+
+def show_board
+  board =  "\n---------------------\n"
+  board += "#{@position[0]} | #{@position[1]} | #{@position[2]}\n"
+  board += "---+---+---\n"
+  board += "#{@position[3]} | #{@position[4]} | #{@position[5]}\n"
+  board += "---+---+---\n"
+  board += "#{@position[6]} | #{@position[7]} | #{@position[8]}\n"
+  board += "---------------------\n"
+  print board
+end
+
+def place_mark(position, mark)
+  attr_accessor :position,  :mark
+
+  if @@board[position] = ''
+    @board[position] = mark
+    current_state
+  end
 end
 
 end
@@ -46,7 +62,7 @@ class Player
       puts "#{@name} chose your symbol :`X` or `O`"
       move = gets.chomp.upcase
   end
-  move
+
 
   def self.info
     puts "The Players Are : #{@@players}"
