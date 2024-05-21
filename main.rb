98194d12e79debc5c79e7526c91844bb92e74db9
boulhouech <slim.boulahouech@gmail.com>
2024-05-21 15:27:21 +0100

    Introduce player_on method and enhance movement functionality by setting coose_X_or_O method

--- main.rb
+++ main.rb
@@ -12,22 +12,39 @@ end
 
 class Player
   @@players = []
-  @@player_movers = []
+  @@player_symbols = []
 
   MAX_PLAYERS = 3
 
   def initialize(name)
     @name = name
 
-    if players.length < MAX_PLAYERS
-      players << @name
+    if @@players.length < MAX_PLAYERS
+      @@players << @name
       #here i should return a method that print that player has entered the game
     else
       raise "This game is only played by two players."
   end
 
-  def player_ready
+  def player_on
     puts "\n#{name} has join the game"
 
+   if @@players.length == 1
+    player_symbols.push(choose_X_or_O)
+   else
+    if player_symbols[0] = 'X'
+      player_symbols.push('O')
+    else
+      player_symbols.push('X')
+   end
   end
+
+  def choose_X_or_O
+    move = ""
+
+    until move = "X" || "O"
+      puts "#{@name} chose your symbol :`X` or `O`"
+      move = gets.chomp.upcase
+  end
+  move
 end
