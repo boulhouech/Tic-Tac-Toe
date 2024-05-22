ca156260187762e8a18b72d2bb2622b70572fbcc
boulhouech <slim.boulahouech@gmail.com>
2024-05-22 20:30:17 +0100

    Add game initialization and start to the main program
    This commit adds the code to initialize the game, including creating the board, players, and getting the player symbols. It also displays the player information using the  method, and then creates and starts the  instance to begin the game.
    
    The main changes are:
    - Initializing the  instance
    - Prompting the user to enter names for player 1 and player 2, and creating  instances for them
    - Getting the player symbols and players from the  class variables
    - Displaying the player information using the  method
    - Creating a  instance and calling the  method to start the game.

--- main.rb
+++ main.rb
@@ -6,45 +6,44 @@ class Game
     @player_moves = [[], []]
   end
 
+  def play_round
+    while true
+      make_move(@players[0], @player_symbols[0])
+      @board.show_board
+      break if check_winner(@player_moves[0]) || @board.board_full?
 
-def play_round
-  while true
-    make_move(@players[0], @player_symbols[0])
-    @board.show_board
-    break if check_winner(@player_moves[0]) || @board.board_full?
-
-    make_move(@players[1], @player_symbols[1])
-    @board.show_board
-    break if check_winner(@player_moves[1]) || @board.board_full?
+      make_move(@players[1], @player_symbols[1])
+      @board.show_board
+      break if check_winner(@player_moves[1]) || @board.board_full?
+    end
   end
-end
 
-def check_winner(moves)
-  winning_combinations = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], # rows
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], # columns
-    [0, 4, 8], [2, 4, 6] # diagonals
-  ]
+  def check_winner(moves)
+    winning_combinations = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], # rows
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], # columns
+      [0, 4, 8], [2, 4, 6] # diagonals
+    ]
 
-  winning_combinations.any? do |combination|
-    combination.all? { |location| moves.include?(location) }
+    winning_combinations.any? do |combination|
+      combination.all? { |location| moves.include?(location) }
+    end
   end
-end
 
-def make_move(player, symbol)
-  puts "#{player} please choose position"
-  chosen_position = gets.chomp.to_i
+  def make_move(player, symbol)
+    puts "#{player} please choose position"
+    chosen_position = gets.chomp.to_i
 
-  if @board.playing_positions.include?(chosen_position)
-    puts 'This position is already used'
-  else
-    @board.playing_positions.push(chosen_position)
-    @board.board_position[chosen_position] = symbol
+    if @board.playing_positions.include?(chosen_position)
+      puts 'This position is already used'
+    else
+      @board.playing_positions.push(chosen_position)
+      @board.board_position[chosen_position] = symbol
 
-    player_index = @players.index(player)
-    @player_moves[player_index] << chosen_position
+      player_index = @players.index(player)
+      @player_moves[player_index] << chosen_position
+    end
   end
-end
 end
 
 class Board
@@ -75,39 +74,36 @@ class Board
   end
 end
 
-
-
 class Player
-  class Player
-    @@players = []
-    @@player_symbols = []
+  @@players = []
+  @@player_symbols = []
 
-    MAX_PLAYERS = 2
+  MAX_PLAYERS = 2
 
-    def initialize(name)
-      @name = name
+  def initialize(name)
+    @name = name
 
-      if @@players.length < MAX_PLAYERS
-        @@players << @name
-        player_on
+    if @@players.length < MAX_PLAYERS
+      @@players << @name
+      player_on
+    else
+      raise 'This game is only played by two players.'
+    end
+  end
+
+  def player_on
+    puts "\n#{@name} has joined the game"
+
+    if @@players.length == 1
+      @@player_symbols.push(choose_x_or_o)
+    else
+      if @@player_symbols[0] == 'X'
+        @@player_symbols.push('O')
       else
-        raise 'This game is only played by two players.'
+        @@player_symbols.push('X')
       end
     end
-
-    def player_on
-      puts "\n#{@name} has joined the game"
-
-      if @@players.length == 1
-        @@player_symbols.push(choose_x_or_o)
-      else
-        if @@player_symbols[0] == 'X'
-          @@player_symbols.push('O')
-        else
-          @@player_symbols.push('X')
-        end
-      end
-    end
+  end
 
   def choose_x_or_o
     move = ''
@@ -118,13 +114,32 @@ class Player
     move
   end
 
-
   def self.info
     puts "The Players Are : #{@@players}"
     puts "\n#{@@players[0]} is #{@@player_symbols[0]}"
     puts "\n#{@@players[1]} is #{@@player_symbols[1]}"
-
   end
-  protected
 end
-end
+
+# Initialize board
+board = Board.new
+
+# Initialize players
+puts "Enter name for player 1:"
+player1_name = gets.chomp
+player1 = Player.new(player1_name)
+
+puts "Enter name for player 2:"
+player2_name = gets.chomp
+player2 = Player.new(player2_name)
+
+# Get player symbols
+player_symbols = Player.class_variable_get(:@@player_symbols)
+players = Player.class_variable_get(:@@players)
+
+# Display player information
+Player.info
+
+# Initialize and start the game
+game = Game.new(board, players, player_symbols)
+game.play_round
