c07ab9a874705e230cb72d40de47300797472cee
boulhouech <slim.boulahouech@gmail.com>
2024-05-22 20:08:17 +0100

    Simplify  method by using class variables
    The previous implementation of the  method used instance variables to track the player moves, which was unnecessary. This commit simplifies the method by using class variables instead, making the code more concise and easier to understand.
    
    The main changes are:
    - Replaced  with
    - Replaced  with

--- main.rb
+++ main.rb
@@ -19,17 +19,17 @@ def play_round
   end
 end
 
-  def check_winner(move)
-    winning_combinations = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8], # rows
-      [0, 3, 6], [1, 4, 7], [2, 5, 8], # columns
-      [0, 4, 8], [2, 4, 6] # diagonals
-    ]
+def check_winner(moves)
+  winning_combinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], # rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], # columns
+    [0, 4, 8], [2, 4, 6] # diagonals
+  ]
 
-    winning_combinations.any? do |combination|
-      combination.all? { |chosen_position| @@board_position[chosen_position] == move }
-    end
+  winning_combinations.any? do |combination|
+    combination.all? { |location| moves.include?(location) }
   end
+end
 
   def make_move(player, move)
     puts "#{player} please choose position"
